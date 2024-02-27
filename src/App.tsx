76ee6357313a4ed8a0c9a384f056e65003736984
Your Name <zai-elina>
2024-02-27 16:14:37 +0300

    Добавление пакета react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import "./index.scss";
 import { Text } from "./components/Text";
+import { Link, Route, Routes } from "react-router-dom";
 
 const App: FC = () => {
   return (
     <div className="app">
-      Заголовок
-      <Text />
+      <Link to={"/"}>main</Link>
+      <br />
+      <Link to={"/about"}>about</Link>
+      <Routes>
+        <Route path={"/"} element={<div>about</div>} />
+        <Route path={"/about"} element={<Text />} />
+      </Routes>
     </div>
   );
 };
