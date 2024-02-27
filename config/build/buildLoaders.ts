e9b4fc25e8ae78257ef52f2db143176bcf5c88be
Your Name <zai-elina>
2024-02-27 15:01:30 +0300

    Добавление React и Scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,5 +7,17 @@ export const buildLoaders = (): RuleSetRule[] => {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader];
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
 };
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from "react-dom";
+import "./index.scss";
+
+render(<h1>Заголовок</h1>, document.getElementById("root"));
