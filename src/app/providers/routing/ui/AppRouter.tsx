2409fe610c2264da8d394728f59d83a2d5b1d8ae
Your Name <zai-elina>
2024-02-28 14:42:29 +0300

    Вынесла routing в provider

---
+++ src/app/providers/routing/ui/AppRouter.tsx
@@ -0,0 +1,25 @@
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { FC, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+interface Props {}
+
+const AppRouter: FC<Props> = (props) => {
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        {routeConfig.map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<Suspense fallback={<>Loading...</>}>{element}</Suspense>}
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
