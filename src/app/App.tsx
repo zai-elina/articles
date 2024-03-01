ba1868967c0d2e645e7683cd11571b26e5d06729
Your Name <zai-elina>
2024-03-01 16:05:44 +0300

    Добавила loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,14 @@ import "./styles/index.scss";
 import { AppRouter } from "./providers/routing";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
+import { PageLoader } from "widgets/PageLoader";
 
 const App: FC = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
+      <Suspense fallback={<PageLoader />}>
         <Navbar />
         <div className="content-page">
           <Sidebar />
