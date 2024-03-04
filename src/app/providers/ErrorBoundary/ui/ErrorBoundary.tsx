441826ee353eccc265090b3961b1f7cc94938405
Your Name <zai-elina>
2024-03-04 16:15:04 +0300

    Класс ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import React, { ErrorInfo, Suspense } from "react";
+import { PageError } from "widgets/PageError";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Suspense fallback="">
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,11 +3,13 @@ import classes from "./Sidebar.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Switchers } from "widgets/Switchers";
 import { Button } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
 
 interface SidebarProps {
   className?: string;
 }
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const { t } = useTranslation();
   const [collapsed, setCollapsed] = useState(false);
 
   const onToggle = () => {
@@ -22,7 +24,7 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
         [className]
       )}
     >
-      <Button onClick={onToggle}>toogle</Button>
+      <Button onClick={onToggle}>{t("Свернуть")}</Button>
       <Switchers className={classes.switchers_bottom} />
     </div>
   );
