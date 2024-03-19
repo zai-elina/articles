18d4cd768a1afec23c39d66418c538542ef98fb7
Your Name <zai-elina>
2024-03-19 15:07:34 +0300

    Стили для Input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Input } from "shared/ui/Input/Input";
 
 const MainPage: FC = () => {
   const { t } = useTranslation();
+  const [value, setValue] = useState("");
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
 
   return (
     <div>
       {t("Главная")}
+      <Input placeholder="Введите текст" value={value} onChange={onChange} />
     </div>
   );
 };
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -9,18 +9,27 @@ interface InputProps
   onChange?: (value: string) => void;
 }
 export const Input = memo((props: InputProps) => {
-  const { className, value, onChange, type = "text", ...otherProps } = props;
+  const {
+    className,
+    value,
+    onChange,
+    type = "text",
+    placeholder,
+    ...otherProps
+  } = props;
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange?.(e.target.value);
   };
 
   return (
-    <div className={classNames(classes.Input, {}, [className])}>
+    <div className={classNames(classes.InputWrapper, {}, [className])}>
       <input
+        className={classes.Input}
         type={type}
         value={value}
         onChange={onChangeHandler}
+        placeholder={placeholder}
         {...otherProps}
       ></input>
     </div>
